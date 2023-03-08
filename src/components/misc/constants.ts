cb1fb790758a24fcc3dd2e112cddcbe835bf0a70
saratovkin <svrvtwkxxn@gmail.com>
2023-03-08 12:57:43 +0100

    feat: build  app for deploy

--- src/components/misc/constants.ts
+++
@@ -1,4 +0,0 @@
-export const MIN_COUNT = "1";
-export const MAX_COUNT = "12";
-export const MIN_YEAR = "1940";
-export const MAX_YEAR = "2020";
--- src/components/nouislider/nouislider.ts
+++
@@ -1,6 +0,0 @@
-import _default from "nouislider";
-import "./nouislider.css";
-
-const noUiSlider = _default;
-
-export default noUiSlider;
